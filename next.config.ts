05f0970300a5d18e5d22e3aee2be07f215a69963
billy.berkouwer <billy@ocove.studio>
2025-07-07 18:14:15 +0100

    update: refactor grid system to use bootstrap as npm package with customized variables declared in global.scss

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api', 'import', 'color-functions', 'global-builtin'],
+  },
+  images: {
+    minimumCacheTTL: 2678400, // 31 days
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.sanity.io' }
+    ]
+  },
 };
 
 export default nextConfig;
