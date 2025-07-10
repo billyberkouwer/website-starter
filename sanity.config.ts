aea023d40f5f7d5b5a3842c8d6d9ce89c709701f
billy.berkouwer <billy@ocove.studio>
2025-07-10 12:29:48 +0100

    update: preview url

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,15 +4,15 @@
  * This configuration is used to for the Sanity Studio that's mounted on the `\app\admin\[[...tool]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
 import { presentationTool } from 'sanity/presentation'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import { apiVersion, dataset, projectId } from './src/sanity/env'
+import { schema } from './src/sanity/schemaTypes'
+import { structure } from './src/sanity/structure'
 
 export default defineConfig({
   basePath: '/admin',
@@ -21,12 +21,13 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
+    structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
     presentationTool({
       previewUrl: {
+        origin: process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000',
         draftMode: {
           enable: '/api/draft',
         },
