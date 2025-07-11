c74efdd7b60a9eb36ff9b16a634cf1c2b9fc995c
billy.berkouwer <billy@ocove.studio>
2025-07-11 12:18:51 +0100

    test: coolify preview

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -4,6 +4,7 @@ import { draftMode } from "next/headers";
 export async function GET(request: NextRequest) {
   const dm = await draftMode();
   dm.enable();
+  console.log(request.nextUrl.searchParams.get("redirect"))
   // Redirect to the page the Studio is previewing
   const url = request.nextUrl.searchParams.get("redirect") || "/";
   return NextResponse.redirect(new URL(url, request.url));
