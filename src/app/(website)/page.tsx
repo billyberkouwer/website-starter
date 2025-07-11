123db6ca75bb1d5c8969c5457b34f333ef1a9931
billy.berkouwer <billy@ocove.studio>
2025-07-11 12:20:13 +0100

    test: node env

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,12 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    console.log(process.env.NODE_ENV);
+  }, []);
   return (
     <div>
       <h1>Next.js/Sanity Starter</h1>
       <div className="container-fluid g-0">
         <div className="row g-0">
-          <div className="col-4"><span>grid</span></div>
-          <div className="col-4"><span>grid</span></div>
-          <div className="col-4"><span>grid</span></div>
+          <div className="col-4">
+            <span>grid</span>
+          </div>
+          <div className="col-4">
+            <span>grid</span>
+          </div>
+          <div className="col-4">
+            <span>grid</span>
+          </div>
         </div>
       </div>
     </div>
