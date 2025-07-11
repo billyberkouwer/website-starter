290ed17e5795253c01397ff455594626fc4374d4
billy.berkouwer <billy@ocove.studio>
2025-07-11 13:09:33 +0100

    test: redirect url

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -6,6 +6,6 @@ export async function GET(request: NextRequest) {
   dm.enable();
   console.log(request.nextUrl.searchParams.get("redirect"))
   // Redirect to the page the Studio is previewing
-  const url = request.nextUrl.searchParams.get("redirect") || "/";
+  // const url = request.nextUrl.searchParams.get("redirect") || "/";
   return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000", request.url));
 } 
