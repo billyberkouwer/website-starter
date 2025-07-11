0a8733df81b30576aa6c0bbd4d8f64c0e7baefb7
billy.berkouwer <billy@ocove.studio>
2025-07-11 13:26:41 +0100

    update: redirect

--- src/app/api/draft/disable/route.ts
+++ src/app/api/draft/disable/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { draftMode } from "next/headers";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const dm = await draftMode();
   dm.disable();
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000"));
 } 
--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { draftMode } from "next/headers";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const dm = await draftMode();
   dm.enable();
-  console.log(request.nextUrl.searchParams.get("redirect"))
+  // console.log(request.nextUrl.searchParams.get("redirect"))
   // Redirect to the page the Studio is previewing
   // const url = request.nextUrl.searchParams.get("redirect") || "/";
-  return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000", request.url));
+  return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000"));
 } 
